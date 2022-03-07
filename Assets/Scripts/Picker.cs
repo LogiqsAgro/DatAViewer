dc4f085fe947698ede9dbf9f251a3c5d9d8dca86
Remco Schoeman <remco.schoeman@logiqs.nl>
2022-03-07 08:39:11 -0500

    Organized project, and added main-menu scene

---
+++ Assets/Scripts/Picker.cs
@@ -0,0 +1,69 @@
+#nullable enable
+
+using UnityEngine;
+
+public class Picker : MonoBehaviour
+{
+    public Camera? Camera;
+    public UnityEngine.UI.Text? PickText;
+    private GameObject? lastHitObject;
+    private Color? hitColor;
+
+    void Update()
+    {
+        if(Camera==null)
+         return;
+
+        var mousePosition = Cursor.lockState == CursorLockMode.Locked
+            ? new Vector3(Camera.pixelWidth, Camera.pixelHeight, 0) * 0.5f
+            : Input.mousePosition;
+
+        Ray ray = Camera.ScreenPointToRay(mousePosition);
+
+        GameObject? hitObject = Physics.Raycast(ray, out var hit) ? hit.transform.gameObject : null;
+
+        if (hitObject != lastHitObject)
+        {
+            OnHitEnd();
+            lastHitObject = hitObject;
+            if (lastHitObject != null)
+            {
+                OnHitStart();
+            }
+            if(PickText!= null)
+                PickText.text = lastHitObject?.name ?? "";
+        }
+    }
+
+    private void OnHitStart()
+    {
+        if (lastHitObject == null) return;
+
+        var meshRenderer = lastHitObject.GetComponent<MeshRenderer>();
+        if (meshRenderer == null) return;
+
+        hitColor = meshRenderer.material.color;
+        meshRenderer.material.color += Color.Lerp(hitColor.Value, Color.white, 0.2f);
+    }
+
+    private void OnHitEnd()
+    {
+        if (hitColor is Color color)
+        {
+            try
+            {
+                if (lastHitObject != null)
+                {
+                    var meshRenderer = lastHitObject.GetComponent<MeshRenderer>();
+                    if (meshRenderer != null) {
+                        meshRenderer.material.color = color;
+                    }
+                }
+            }
+            catch (MissingReferenceException) { }
+            hitColor = null;
+        }
+        lastHitObject = null;
+    }
+
+}
--- README.md
+++ README.md
@@ -8,5 +8,14 @@ You can fly around with W, A, S, D, LCTRL, SPACE. LSHIFT accellerates you. ESC u
 
 ## Authoring details
 
-Built with [Unity](https://unity.com/download) 2020.3.25f1 and Visual Studio 2022 on Windows 10 by [Mick van Gelderen](https://github.com/mickvangelderen).
+Built with:
 
+* [Unity](https://unity.com/download) 2020.3.301f with the following packages:
+  * [com.unity.ui.builder](https://docs.unity3d.com/Packages/com.unity.ui.builder@1.0/manual/index.html)
+
+* Visual Studio 2022 on Windows 10
+
+## Authors
+
+* [Mick van Gelderen](https://github.com/mickvangelderen).
+* [Remco Schoeman](https://github.com/resc)
